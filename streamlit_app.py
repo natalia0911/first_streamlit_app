9934aeca826cfc19147205d1737c6bdf21c75e8a
Hawk-chan <30329240+natalia0911@users.noreply.github.com>
2023-07-31 20:40:34 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title('New healthy Menu')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and blueberry Oatmeal')
-streamlit.text('Spinach and rocket smothie')
-streamlit.text('Hard boiled free-range eggs')
+streamlit.text('🥣 Omega 3 and blueberry Oatmeal')
+streamlit.text('🥗 Spinach and rocket smothie')
+streamlit.text('🐔 Hard boiled free-range eggs')
+streamlit.text('🥑🍞 Avocado toast')
